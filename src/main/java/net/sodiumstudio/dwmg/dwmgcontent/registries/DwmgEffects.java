5af4b6b589f8dfa56a9bba36858e31fe1b01daf3
SodiumZH <hlzhang_1998@outlook.com>
2023-03-24 03:22:35 +0800

    Update DwmgEffects.java

--- src/main/java/net/sodiumstudio/dwmg/dwmgcontent/registries/DwmgEffects.java
+++ src/main/java/net/sodiumstudio/dwmg/dwmgcontent/registries/DwmgEffects.java
@@ -1,7 +1,6 @@
 package net.sodiumstudio.dwmg.dwmgcontent.registries;
 
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,7 +13,6 @@ public class DwmgEffects {
 
 	public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, Dwmg.MOD_ID);
 	
-
 	// Registry body
 	
 	public static final RegistryObject<MobEffect> UNDEAD_AFFINITY = EFFECTS.register("undead_affinity", () -> new EffectUndeadAffinity());
