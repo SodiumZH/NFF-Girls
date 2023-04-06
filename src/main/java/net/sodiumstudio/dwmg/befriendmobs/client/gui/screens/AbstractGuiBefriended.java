6bf7141a991db718543d85bdd45cd2653017ddb6
SodiumZH <hlzhang_1998@outlook.com>
2023-04-06 20:16:10 +0800

    Hatred refactor planned

--- src/main/java/net/sodiumstudio/dwmg/befriendmobs/client/gui/screens/AbstractGuiBefriended.java
+++ src/main/java/net/sodiumstudio/dwmg/befriendmobs/client/gui/screens/AbstractGuiBefriended.java
@@ -1,5 +1,15 @@
 package net.sodiumstudio.dwmg.befriendmobs.client.gui.screens;
 
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.sodiumstudio.dwmg.befriendmobs.entity.IBefriendedMob;
+import net.sodiumstudio.dwmg.befriendmobs.inventory.AbstractInventoryMenuBefriended;
+import net.sodiumstudio.dwmg.befriendmobs.util.exceptions.UnimplementedException;
+import net.sodiumstudio.dwmg.befriendmobs.util.math.IntVec2;
+
+import java.text.DecimalFormat;
+
+
 import javax.annotation.Nullable;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -12,6 +22,7 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.contents.LiteralContents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -82,6 +93,15 @@ public abstract class AbstractGuiBefriended extends AbstractContainerScreen<Abst
 	public void addHealthInfo(PoseStack poseStack, IntVec2 position)
 	{
 		addHealthInfo(poseStack, position, 4210752);
+	}
+	
+	public void addAttributeInfo(PoseStack poseStack, IntVec2 position, int color)
+	{
+		DecimalFormat df = new DecimalFormat("##.##");
+		float hp = mob.asMob().getHealth();
+		double atk = mob.asMob().getAttributeValue(Attributes.ATTACK_DAMAGE);
+		double def = mob.asMob().getAttributeValue(Attributes.ARMOR);
+		throw new UnimplementedException();
 	}
 	
 }
