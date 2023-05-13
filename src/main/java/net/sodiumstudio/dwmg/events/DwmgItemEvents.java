938cdb94b7ff0f09648cffa5867fe518ba81bb97
SodiumZH <hlzhang_1998@outlook.com>
2023-05-13 13:27:03 +0800

    Necromancer's Wand repair
    16 durability each Death Crystal Powder

--- src/main/java/net/sodiumstudio/dwmg/events/DwmgItemEvents.java
+++ src/main/java/net/sodiumstudio/dwmg/events/DwmgItemEvents.java
@@ -3,6 +3,15 @@ package net.sodiumstudio.dwmg.events;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +23,7 @@ import net.sodiumstudio.befriendmobs.util.InfoHelper;
 import net.sodiumstudio.befriendmobs.util.NbtHelper;
 import net.sodiumstudio.befriendmobs.util.Wrapped;
 import net.sodiumstudio.dwmg.Dwmg;
+import net.sodiumstudio.dwmg.registries.DwmgItems;
 
 @Mod.EventBusSubscriber(modid = Dwmg.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DwmgItemEvents
@@ -72,4 +82,38 @@ public class DwmgItemEvents
 			event.setCanceled(true);
 		}
 	}
+
+	protected static final UUID SHARPNESS_MODIFIER_UUID = UUID.fromString("9c12b503-63c0-43e6-bd30-d7aae9818c99");
+	
+	/**
+	 * Add sharpness atk modifier to befriended mobs
+	 */
+	@SubscribeEvent
+	public static void onBefriendedMainHandItemChange(CItemStackMonitor.ChangeEvent event)
+	{
+		event.living.getAttribute(Attributes.ATTACK_DAMAGE).removeModifier(SHARPNESS_MODIFIER_UUID);
+		int lv = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SHARPNESS, event.to);
+		if (lv > 0)
+		{
+			event.living.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(new AttributeModifier(
+					SHARPNESS_MODIFIER_UUID, "sharpness_modifier", 0.5d + 0.5d * (double) lv, AttributeModifier.Operation.ADDITION));
+		}		
+	}
+	
+	@SubscribeEvent
+	public static void onAnvilChange(AnvilUpdateEvent event)
+	{
+		// Necromancer's Wand fixing
+		if (event.getLeft().is(DwmgItems.NECROMANCER_WAND.get()) 
+				&& event.getRight().is(DwmgItems.DEATH_CRYSTAL_POWDER.get())
+				&& event.getLeft().getDamageValue() > 0)
+		{
+			ItemStack out = event.getLeft().copy();
+			event.setCost(1);
+			event.setMaterialCost(1);
+			out.setDamageValue(event.getLeft().getDamageValue() - 16);
+			event.setOutput(out);
+		}
+	}
+
 }
