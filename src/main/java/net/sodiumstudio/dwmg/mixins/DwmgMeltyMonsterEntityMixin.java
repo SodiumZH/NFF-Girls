489d1e467e86fabb3160c8a86ac3ec72bbb82bf2
SodiumZH <hlzhang_1998@outlook.com>
2023-11-24 19:45:50 +0800

    misc

--- src/main/java/net/sodiumstudio/dwmg/mixins/DwmgMeltyMonsterEntityMixin.java
+++ src/main/java/net/sodiumstudio/dwmg/mixins/DwmgMeltyMonsterEntityMixin.java
@@ -2,13 +2,12 @@ package net.sodiumstudio.dwmg.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.github.mechalopa.hmag.world.entity.MeltyMonsterEntity;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.sodiumstudio.dwmg.events.hooks.DwmgHooks;
@@ -18,14 +17,10 @@ import net.sodiumstudio.nautils.mixins.NaUtilsMixin;
 public class DwmgMeltyMonsterEntityMixin implements NaUtilsMixin<MeltyMonsterEntity>
 {
 
-	@Redirect(method = "aiStep()V", at = @At(value = "INVOKE", 
+	@WrapWithCondition(method = "aiStep()V", at = @At(value = "INVOKE", 
 			target = "Lnet/minecraft/world/level/Level;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
-	private boolean onSetFire(Level caller, BlockPos pos, BlockState state)
+	private boolean canSetFire(Level instance, BlockPos pos, BlockState blockstate)
 	{
-		if (!MinecraftForge.EVENT_BUS.post(new DwmgHooks.MeltyMonsterSetFireEvent(get())))
-		{
-			return false;
-		}
-		else return caller.setBlockAndUpdate(pos, BaseFireBlock.getState(caller, pos));
+		return !MinecraftForge.EVENT_BUS.post(new DwmgHooks.MeltyMonsterSetFireEvent(get(), pos, blockstate));	
 	}
 }
