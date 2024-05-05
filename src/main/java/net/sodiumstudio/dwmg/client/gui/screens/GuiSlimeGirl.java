c353e095ec12766b4259b815f10291de82e5c60c
SodiumZH <hlzhang_1998@outlook.com>
2024-05-05 17:52:21 +0800

    fix GuiPos
    adapting to immutable GuiPos

--- src/main/java/net/sodiumstudio/dwmg/client/gui/screens/GuiSlimeGirl.java
+++ src/main/java/net/sodiumstudio/dwmg/client/gui/screens/GuiSlimeGirl.java
@@ -3,8 +3,10 @@ package net.sodiumstudio.dwmg.client.gui.screens;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.core.Vec3i;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Inventory;
 import net.sodiumstudio.befriendmobs.entity.befriended.IBefriendedMob;
 import net.sodiumstudio.befriendmobs.inventory.BefriendedInventoryMenu;
@@ -54,21 +56,58 @@ public class GuiSlimeGirl extends GuiPreset0
 	public MutableComponent getDefaultColorInfo()
 	{
 		LinearColor color = sg.getColorLinear();
-		Vec3i rgb = color.toRGB();
 		String colorKey = "color.dwmg.html." + HtmlColors.getNearestHtmlColor(color);
-		String rgbInfo = " (R" + Integer.toString(rgb.getX()) + ", G" + Integer.toString(rgb.getY()) + ", B" + Integer.toString(rgb.getZ()) +")";
 		return ComponentBuilder.create().appendTranslatable("info.dwmg.gui_slime_color").appendText(": ")
-				.appendTranslatable(colorKey).appendText(rgbInfo).build().withStyle(Style.EMPTY.withColor(color.toCode()));
+				.appendTranslatable(colorKey).build().withStyle(Style.EMPTY.withColor(color.toCode()));
+	}
+	
+	public MutableComponent getColorRGBInfo()
+	{
+		LinearColor color = sg.getColorLinear();
+		Vec3i rgb = color.toRGB();
+		String rgbInfo = " (R" + Integer.toString(rgb.getX()) + ", G" + Integer.toString(rgb.getY()) + ", B" + Integer.toString(rgb.getZ()) +")";
+		return InfoHelper.createText(rgbInfo).withStyle(Style.EMPTY.withColor(color.toCode()));
+	}
+	
+	@Override
+	public void addAttributeInfo(PoseStack poseStack, GuiPos position, int color, int textRowWidth)
+	{
+		this.addBasicAttributeInfo(poseStack, position, color, textRowWidth);
+		position = position.addY(textRowWidth * 2);
+		this.addFavorabilityAndLevelInfo(poseStack, position, color, textRowWidth);
+	}
+	
+	@Override
+	public void addBasicAttributeInfo(PoseStack poseStack, GuiPos position, int color, int textRowWidth)
+	{
+		GuiPos pos = position;
+		String hp = Integer.toString(Math.round(mob.asMob().getHealth()));
+		String maxHp = Long.toString(Math.round(mob.asMob().getAttributeValue(Attributes.MAX_HEALTH)));
+		String atk = Long.toString(Math.round(mob.asMob().getAttributeValue(Attributes.ATTACK_DAMAGE)));
+		String def = Long.toString(Math.round(mob.asMob().getAttributeValue(Attributes.ARMOR)));
+		Component hpcomp = InfoHelper.createTranslatable("info.befriendmobs.gui_health")
+				.append(InfoHelper.createText(": " + hp + " / " + maxHp));
+		Component atkcomp = InfoHelper.createTranslatable("info.befriendmobs.gui_atk")
+				.append(InfoHelper.createText(": " + atk));
+		Component defcomp = InfoHelper.createTranslatable("info.befriendmobs.gui_armor")
+				.append(InfoHelper.createText(": " + def));
+		Component atkDefComp = ComponentBuilder.create().append(atkcomp).appendText(" | ")
+				.append(defcomp).build();
+		font.draw(poseStack, hpcomp, pos.x, pos.y, color);
+		pos = pos.addY(textRowWidth);
+		font.draw(poseStack, atkDefComp, pos.x, pos.y, color);
 	}
 	
 	@Override
 	public void addFavorabilityAndLevelInfo(PoseStack poseStack, GuiPos position, int color, int textRowWidth)
 	{
 		font.draw(poseStack, getDefaultLevelAndExpInfo(), position.x, position.y, color);
-		position.addY(textRowWidth);
+		position = position.addY(textRowWidth);
 		font.draw(poseStack, getDefaultFavInfo(), position.x, position.y, color);
-		position.addY(textRowWidth);
+		position = position.addY(textRowWidth);
 		font.draw(poseStack, getDefaultColorInfo(), position.x, position.y, color);
+		position = position.addY(textRowWidth);
+		font.draw(poseStack, getColorRGBInfo(), position.x, position.y, color);
 	}
 
 	@Override
