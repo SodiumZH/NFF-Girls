7fd9355e700486e785932d2f74f8eee08943eb9c
SodiumZH <hlzhang_1998@outlook.com>
2024-05-24 17:05:02 +0800

    Some dwmg trade setup

--- src/main/java/net/sodiumstudio/dwmg/registries/DwmgCapabilities.java
+++ src/main/java/net/sodiumstudio/dwmg/registries/DwmgCapabilities.java
@@ -10,6 +10,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.sodiumstudio.dwmg.entities.capabilities.CFavorabilityHandler;
 import net.sodiumstudio.dwmg.entities.capabilities.CLevelHandler;
 import net.sodiumstudio.dwmg.entities.capabilities.CUndeadMob;
+import net.sodiumstudio.dwmg.entities.vanillatrade.CDwmgTradeHandler;
+import net.sodiumstudio.nautils.entity.vanillatrade.CVanillaMerchant;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DwmgCapabilities
@@ -18,7 +20,8 @@ public class DwmgCapabilities
 	public static Capability<CUndeadMob> CAP_UNDEAD_MOB = CapabilityManager.get(new CapabilityToken<>(){});
 	public static Capability<CFavorabilityHandler> CAP_FAVORABILITY_HANDLER = CapabilityManager.get(new CapabilityToken<>(){});
 	public static Capability<CLevelHandler> CAP_LEVEL_HANDLER = CapabilityManager.get(new CapabilityToken<>(){});
-
+	public static Capability<CDwmgTradeHandler> CAP_TRADE_HANDLER = CapabilityManager.get(new CapabilityToken<>(){});
+	
 	// Register capabilities
 	@SubscribeEvent
 	public static void register(RegisterCapabilitiesEvent event)
@@ -27,6 +30,7 @@ public class DwmgCapabilities
 		event.register(CUndeadMob.class);
 		event.register(CFavorabilityHandler.class);
 		event.register(CLevelHandler.class);
+		event.register(CDwmgTradeHandler.class);
 	}
 	
 	
