dba66f5ae5f4da3494c78cefea3015c1adae3acf
SodiumZH <hlzhang_1998@outlook.com>
2024-06-26 01:27:58 +0800

    registry tiny changes
    adapting dependents

--- src/main/java/net/sodiumstudio/dwmg/registries/DwmgEntityTypes.java
+++ src/main/java/net/sodiumstudio/dwmg/registries/DwmgEntityTypes.java
@@ -1,28 +1,11 @@
 package net.sodiumstudio.dwmg.registries;
 
-import java.util.HashMap;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
-import javax.annotation.Nonnull;
-
-import com.github.mechalopa.hmag.HMaG;
-import com.github.mechalopa.hmag.client.renderer.GlaryadRenderer;
-import com.github.mechalopa.hmag.world.entity.AlrauneEntity;
-import com.github.mechalopa.hmag.world.entity.CursedDollEntity;
-import com.github.mechalopa.hmag.world.entity.DodomekiEntity;
-import com.github.mechalopa.hmag.world.entity.GlaryadEntity;
-import com.github.mechalopa.hmag.world.entity.projectile.PoisonSeedEntity;
-
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -426,7 +409,7 @@ public class DwmgEntityTypes {
 	
 	// ========== Utilities ============ //
 	
-	public static <T extends LivingEntity> RegistryObject<EntityType<T>> registerBM(DeferredRegister<EntityType<?>> registry, String modId, String regName, 
+	protected static <T extends LivingEntity> RegistryObject<EntityType<T>> registerBM(DeferredRegister<EntityType<?>> registry, String modId, String regName, 
 			EntityType.EntityFactory<T> creator, MobCategory category, Function<EntityType.Builder<T>, EntityType.Builder<T>> builderModifier)
 	{
 		return registry.register(regName, () -> builderModifier.apply(EntityType.Builder
--- src/main/java/net/sodiumstudio/dwmg/registries/DwmgHealingItems.java
+++ src/main/java/net/sodiumstudio/dwmg/registries/DwmgHealingItems.java
@@ -1,55 +1,58 @@
 package net.sodiumstudio.dwmg.registries;
 
 import java.util.HashMap;
-import java.util.function.Function;
+import java.util.Map;
 
 import com.github.mechalopa.hmag.registry.ModItems;
 
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.sodiumstudio.befriendmobs.entity.capability.HealingItemTable;
-import net.sodiumstudio.nautils.NaContainerUtils;
-import net.sodiumstudio.nautils.containers.MapPair;
+import net.sodiumstudio.dwmg.Dwmg;
 
 public class DwmgHealingItems
 {
 
-	// Full-static
+	private static final Map<ResourceLocation, HealingItemTable> HEALING_ITEMS = new HashMap<>();
 	
-	public static final HealingItemTable NONE = HealingItemTable.builder().build();
+	public static HealingItemTable register(String key, HealingItemTable value)
+	{
+		HEALING_ITEMS.put(new ResourceLocation(Dwmg.MOD_ID, key), value);
+		return value;
+	}
+
+	public static final HealingItemTable NONE = register("none", HealingItemTable.builder().build());
 	
-	public static final HealingItemTable UNDEAD = HealingItemTable.builder()
+	public static final HealingItemTable UNDEAD = register("undead", HealingItemTable.builder()
 		.add(ModItems.SOUL_POWDER.get(), 5f)
 		.add(ModItems.SOUL_APPLE.get(), 15f)
-		.build();
+		.build());
 	
-	public static final HealingItemTable CREEPER = HealingItemTable.builder()
+	public static final HealingItemTable CREEPER = register("creeper", HealingItemTable.builder()
 		.add(Items.GUNPOWDER, 5.0f)
 		.add(Items.REDSTONE, 5.0f)
 		.add(Items.REDSTONE_BLOCK, 15.0f)
-		.build();
+		.build());
 	
-	public static final HealingItemTable ENDERMAN = HealingItemTable.builder()
+	public static final HealingItemTable ENDERMAN = register("enderman", HealingItemTable.builder()
 		.add(Items.ENDER_EYE, 5.0f)
 		.add(ModItems.ANCIENT_STONE.get(), 15.0f)
-		.build();
+		.build());
 
-	public static final HealingItemTable GHAST = HealingItemTable.builder()
+	public static final HealingItemTable GHAST = register("ghast", HealingItemTable.builder()
 		.add(Items.GUNPOWDER, 5.0f)
 		.add(ModItems.SOUL_POWDER.get(), 5f)
 		.add(ModItems.SOUL_APPLE.get(), 15f)
-		.build();
+		.build());
 	
-	public static final HealingItemTable PLANT = HealingItemTable.builder()
+	public static final HealingItemTable PLANT = register("plant", HealingItemTable.builder()
 		.add(Items.WHEAT_SEEDS, 2.0f)
 		.add(Items.BONE_MEAL, 5.0f)
 		.add(Items.SPORE_BLOSSOM, 15f)
 		.add(ModItems.MYSTERIOUS_PETAL.get(), mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable ANIMAL = HealingItemTable.builder()
+	public static final HealingItemTable ANIMAL = register("animal", HealingItemTable.builder()
 		.add(Items.COOKIE, 5f)
 		.add(Items.COOKED_CHICKEN, 8f)
 		.add(Items.COOKED_RABBIT, 8f)
@@ -59,45 +62,45 @@ public class DwmgHealingItems
 		.add(ModItems.COOKED_RAVAGER_MEAT.get(), 30f)
 		.add("twilightforest:cooked_meef", 8f)
 		.add(Items.GOLDEN_APPLE, mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable BEE = HealingItemTable.builder()
+	public static final HealingItemTable BEE = register("bee", HealingItemTable.builder()
 		.add(Items.HONEY_BOTTLE, 5.0f)
 		.add(Items.HONEYCOMB, 10.0f)
 		.add(Items.HONEY_BLOCK, 15.0f)
 		.add(ModItems.MYSTERIOUS_PETAL.get(), mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable GENERAL_HUMANOID_0 = HealingItemTable.builder()
+	public static final HealingItemTable GENERAL_HUMANOID_0 = register("general_humanoid_0", HealingItemTable.builder()
 		.add(Items.APPLE, 5f)
 		.add(Items.COOKIE, 5f)
 		.add(Items.PUMPKIN_PIE, 15f)
 		.add(ModItems.LEMON.get(), 10f)
 		.add(ModItems.LEMON_PIE.get(), 20f)
 		.add(Items.GOLDEN_APPLE, mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable SNOWMAN = HealingItemTable.builder()
+	public static final HealingItemTable SNOWMAN = register("snowman", HealingItemTable.builder()
 		.add(Items.SNOWBALL, 2f)
 		.add(Items.SNOW_BLOCK, 5f)
 		.add(Items.PUMPKIN_PIE, 15f)
 		.add(Items.GOLDEN_APPLE, mob -> mob.getMaxHealth())
-		.build();
+		.build());
 
-	public static final HealingItemTable SLIME = HealingItemTable.builder()
+	public static final HealingItemTable SLIME = register("slime", HealingItemTable.builder()
 		.add(Items.SLIME_BALL, 5f)
 		.add(DwmgItems.MAGICAL_GEL_BALL.get(), 15f)
 		.add(ModItems.CUBIC_NUCLEUS.get(), mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable CRIMSON = HealingItemTable.builder()
+	public static final HealingItemTable CRIMSON = register("crimson", HealingItemTable.builder()
 		.add(Items.CRIMSON_FUNGUS, 5f)
 		.add(Items.NETHER_WART, 5f)
 		.add(Items.SHROOMLIGHT, 15f)
 		.add(Items.GOLDEN_APPLE, mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable CLOTH_DOLL = HealingItemTable.builder()
+	public static final HealingItemTable CLOTH_DOLL = register("cloth_doll", HealingItemTable.builder()
 		.add(Items.STRING, 2f)
 		.add(Items.WHITE_WOOL, 5f)
 		.add(Items.LIGHT_GRAY_WOOL, 5f)
@@ -107,20 +110,20 @@ public class DwmgHealingItems
 		.add(Items.LAPIS_LAZULI, 10f)
 		.add(Items.EMERALD, 15f)
 		.add(ModItems.CUBIC_NUCLEUS.get(), mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable BLAZE = HealingItemTable.builder()
+	public static final HealingItemTable BLAZE = register("blaze", HealingItemTable.builder()
 		.add(Items.COAL, 5f)
 		.add(Items.FIRE_CHARGE, 10f)
 		.add(Items.BLAZE_POWDER, 15f)
 		.add(ModItems.BURNING_CORE.get(), mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
-	public static final HealingItemTable CLAY_DOLL = HealingItemTable.builder()
+	public static final HealingItemTable CLAY_DOLL = register("clay_doll", HealingItemTable.builder()
 		.add(Items.CLAY_BALL, 2f)
 		.add(Items.LAPIS_LAZULI, 5f)
 		.add(ModItems.ANCIENT_STONE.get(), 15f)
 		.add(Items.GOLDEN_APPLE, mob -> mob.getMaxHealth())
-		.build();
+		.build());
 	
 }
